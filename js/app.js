bf0ee241a38231714288e79355854a558a8cd3b2
voigtvisuals <166148823+voigtvisuals@users.noreply.github.com>
2024-04-14 17:35:55 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,7 +2,119 @@ const hamMenu = document.querySelector(".ham-menu");
 
 const offScreenMenu = document.querySelector(".off-screen-menu");
 
+const menuScrollPrevent = document.querySelector("body");
+
+const smoothnessAdder = document.querySelector(".heading-1");
+
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
+  menuScrollPrevent.classList.toggle("scroll-prevent");
+  smoothnessAdder.classList.toggle("smoothness-adder")
 });
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+        } 
+    else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el)=> observer.observe(el));
+
+
+// project gallery def not copied either
+
+var modalInfo = {
+  1: {
+    title: "Project 1",
+    info: "...",
+    link: "#",
+    github: "#"
+  },
+  2: {
+    title: "Project 2",
+    info: "...",
+    link: "#",
+    github: "#"
+  },
+  3: {
+    title: "Project 3",
+    info: "...",
+    link: "#",
+    github: "#"
+  },
+  4: {
+    title: "Project 4",
+    info: "....",
+    link: "#",
+    github: "#"
+  },
+  5: {
+    title: "Project 5",
+    info: "...",
+    link: "#",
+    github: "#"
+  },
+  6: {
+    title: "Project 6",
+    info: "...",
+    link: "#",
+    github: "#"
+  }
+};
+
+// Get the modal
+var modal = document.getElementById('preview');
+
+// button that opens the modal
+var btn = document.getElementsByClassName("button");
+
+// <span> that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// open modal 
+for(let i = 0; i < btn.length; i++){
+  btn[i].addEventListener("click", function() {
+    var project = btn[i].parentElement;
+    openModal(project);
+  })
+};
+
+function openModal(project){
+  var id = project.id;
+  var img = project.getElementsByTagName("img")[0].src;
+  fillOut(id, img);
+  modal.style.display = "block";
+  document.getElementsByClassName("modal-content")[0].classList.add("scale");
+}
+
+function fillOut(id, img){
+  document.getElementById("title").innerHTML = modalInfo[id].title;
+  document.getElementById("info").innerHTML = modalInfo[id].info;
+  document.getElementById("img").src = img;
+  document.getElementById("live").onclick = function(){
+    window.open(modalInfo[id].link,'_blank');
+  }
+  document.getElementById("github").onclick = function(){
+    window.open(modalInfo[id].github,'_blank');
+  }
+}
+
+// close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
